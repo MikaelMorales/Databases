5ec11169f8402b7d807a36bde9ff3e719db84cf5
MikaelMorales <morales.mikael@outlook.com>
2018-03-24 12:12:32 +0100

    Select with vector size

--- Project1/cs422-project1/src/main/java/ch/epfl/dias/ops/vector/Select.java
+++ Project1/cs422-project1/src/main/java/ch/epfl/dias/ops/vector/Select.java
@@ -3,9 +3,8 @@ package ch.epfl.dias.ops.vector;
 import ch.epfl.dias.ops.BinaryOp;
 import ch.epfl.dias.store.column.DBColumn;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Select implements VectorOperator {
 
@@ -13,6 +12,8 @@ public class Select implements VectorOperator {
 	private BinaryOp op;
 	private int fieldNo;
 	private int value;
+	private DBColumn[] cacheColumns;
+	private int vectorSize = -1;
 
 	public Select(VectorOperator child, BinaryOp op, int fieldNo, int value) {
 		this.child = child;
@@ -28,25 +29,55 @@ public class Select implements VectorOperator {
 
 	@Override
 	public DBColumn[] next() {
-		DBColumn[] columns = child.next();
-		Set<Integer> validIndices = new HashSet<>();
-		DBColumn[] result = new DBColumn[columns.length];
+		DBColumn[] columns;
+		int currentSize = 0;
 
-		while (!columns[0].eof && validIndices.isEmpty()) {
-			validIndices = new HashSet<>();
-			DBColumn filteredColumn = getValidIndices(columns[fieldNo], validIndices);
-			for (int i = 0; i < result.length; i++) {
-				if (i != fieldNo)
-					result[i] = filterColumn(columns[i], validIndices);
-				else
-					result[i] = filteredColumn;
-			}
-			if (validIndices.isEmpty())
-				columns = child.next();
+		if(cacheColumns != null) {
+			columns = cacheColumns.clone();
+			cacheColumns = null;
+		} else {
+			columns = child.next();
 		}
 
 		if (columns[0].eof) {
+			return columns;
+		}
+
+		if (vectorSize == -1)
+			vectorSize = columns[0].attributes == null ? 0 : columns[0].attributes.length;
+
+		DBColumn[] result = new DBColumn[columns.length];
+
+		while (!columns[0].eof && vectorSize != currentSize) {
+			List<Integer> validIndices = new ArrayList<>();
+			Integer[] attr = columns[fieldNo].getAsInteger();
+
+			int index = 0;
+			while (index < attr.length && currentSize < vectorSize) {
+				if(isValidIndex(attr[index])) {
+					validIndices.add(index);
+					currentSize++;
+				}
+				index++;
+			}
+
 			for (int i = 0; i < result.length; i++) {
+				result[i] = fillColumns(result[i], columns[i], validIndices);
+			}
+
+			if (index != attr.length) {
+				cacheColumns = new DBColumn[columns.length];
+				for (int i = 0; i < result.length; i++) {
+					cacheColumns[i] = fillCache(columns[i], index);
+				}
+			}
+
+			if (vectorSize != currentSize)
+				columns = child.next();
+		}
+
+		if (columns[0].eof && result[0].attributes.length == 0) {
+			for (int i=0; i < columns.length; i++) {
 				result[i] = new DBColumn();
 			}
 		}
@@ -58,47 +89,45 @@ public class Select implements VectorOperator {
 		child.close();
 	}
 
-	private DBColumn filterColumn(DBColumn col, Set<Integer> validIndex) {
-		Object[] attr = col.attributes;
-		Object[] filteredAttr = IntStream.range(0, attr.length)
-				.filter(validIndex::contains)
-				.mapToObj(i -> attr[i])
-				.toArray();
-		return new DBColumn(filteredAttr, col.type);
+	private DBColumn fillCache(DBColumn col, Integer start) {
+		Object[] cache = new Object[col.attributes.length - start];
+		System.arraycopy(col.attributes, start, cache, 0, col.attributes.length - start);
+		return new DBColumn(cache, col.type);
 	}
 
-	private DBColumn getValidIndices(DBColumn col, Set<Integer> validIndex) {
-		Integer[] attr = col.getAsInteger();
-		Object[] filteredAttr = IntStream.range(0, attr.length)
-				.filter(i -> {
-					boolean isValid = false;
-					switch (op) {
-						case GE:
-							isValid = attr[i] >= value;
-							break;
-						case GT:
-							isValid = attr[i] > value;
-							break;
-						case EQ:
-							isValid = attr[i] == value;
-							break;
-						case LE:
-							isValid = attr[i] <= value;
-							break;
-						case LT:
-							isValid = attr[i] < value;
-							break;
-						case NE:
-							isValid = attr[i] != value;
-							break;
-					}
-					if (isValid)
-						validIndex.add(i);
-					return isValid;
-				})
-				.mapToObj(i -> attr[i])
-				.toArray();
+	private DBColumn fillColumns(DBColumn res, DBColumn col, List<Integer> validIndices) {
+		int size = 0;
+		if (res != null && res.attributes != null)
+			size = res.attributes.length;
+		Object[] result = new Object[size + validIndices.size()];
+		if (size != 0) {
+			System.arraycopy(res.attributes, 0, result, 0, size);
+		}
 
-		return new DBColumn(filteredAttr, col.type);
+		Object[] attr = col.attributes;
+		for (int i=0; i < validIndices.size(); i++) {
+			result[size+i] = attr[validIndices.get(i)];
+		}
+
+		return new DBColumn(result, col.type);
+	}
+
+	private boolean isValidIndex(Integer i) {
+		switch (op) {
+			case GE:
+				return i >= value;
+			case GT:
+				return i > value;
+			case EQ:
+				return i == value;
+			case LE:
+				return i <= value;
+			case LT:
+				return i < value;
+			case NE:
+				return i != value;
+			default:
+				return false;
+		}
 	}
 }
